4657dddb4b2d9e0c18b2c2492189a008f2cf3e60
Gubbstah <matstblom@gmail.com>
2025-11-23 21:30:54 +0100

    Updated stripe-config.ts

--- src/lib/stripe-config.ts
+++ src/lib/stripe-config.ts
@@ -1,5 +1,6 @@
 export const VIBZ_PRODUCT = {
-  priceId: 'price_1RIx04CpopuapeXYakHEyNvJ',
-  mode: 'payment' as const,
+  priceId: 'price_1RIx20CpopuapeXYakHEyNvJ',
   name: '$VIBZ',
+  description: '$VIBZ are used to pay in Vibz World. Your payment will be used both to pay for your use of the Vibz World and to make Vibz World better. We thank you for that. You can also earn $VIBZ by contributing to Vibz World ecosystem - join us!',
+  mode: 'payment' as const,
 };
